dcb29431643a87dab70f047a45b3e2f6ed49eddc
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-08-17 09:20:05 +0530

    4th commit

--- Handwritten/handwritten.py
+++ Handwritten/handwritten.py
@@ -1,7 +1,2 @@
 import pywhatkit
 pywhatkit.text_to_handwriting('The Internet of Things (IoT) technology will connect the devices over the internet. The devices such as wearables, smartphones, sensors, and other devices will involve in active data collect', rgb=[0,0,0])
-
-
-
-
-
